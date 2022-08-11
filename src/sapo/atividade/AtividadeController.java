6dfc015a5b965e0c04d7d50032788d83ea28ba3d
jgluiggi <jgluiggi@protonmail.com>
2022-08-10 21:39:11 -0300

    Esqueleto do controller feito, requer devidas implementações.

--- src/sapo/atividade/AtividadeController.java
+++ src/sapo/atividade/AtividadeController.java
@@ -1,6 +1,38 @@
 package sapo.atividade;
 
 public class AtividadeController {
+
+    private final AtividadeService atividadeService;
+
     public AtividadeController(AtividadeService atividadeService) {
+        this.atividadeService = atividadeService;
+    }
+    //TODO Implementar TUDO
+    public String cadastrarAtividade(String nome, String descricao, String cpf) {
+        return "vou fazer carai";
+    }
+
+    public void encerrarAtividade(String atividadeId) {
+
+    }
+
+    public void desativarAtividade(String atividadeId) {
+
+    }
+
+    public void reabrirAtividade(String atividadeId) {
+
+    }
+
+    public String exibirAtividade(String atividadeId) {
+        return "vou fazer ainda intellij, fique calmo";
+    }
+
+    public void alterarDescricaoAtividade(String atividadeId, String descricao) {
+
+    }
+
+    public void alterarResponsavelAtividade(String atividadeId, String cpf) {
+
     }
 }
