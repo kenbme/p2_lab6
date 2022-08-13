9d16876af9dc327001720bfd6cbc06bd5980504f
kenbme <guilhermekenned1@gmail.com>
2022-08-13 00:16:33 -0300

    Refatorando responsabilidades.

--- src/sapo/pessoa/PessoaRepository.java
+++ src/sapo/pessoa/PessoaRepository.java
@@ -1,12 +1,11 @@
 package sapo.pessoa;
 
 import java.util.HashMap;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class PessoaRepository {
 
-    HashMap<String, Pessoa> pessoas;
+    private final HashMap<String, Pessoa> pessoas;
 
     public PessoaRepository() {
         pessoas = new HashMap<>();
@@ -20,61 +19,8 @@ public class PessoaRepository {
         pessoas.put(pessoa.getCPF(), pessoa);
     }
 
-    public void cadastrarPessoa(String cpf, String nome, String[] habilidades) {
-        Pessoa pessoa = pessoas.get(cpf);
-        if (pessoa != null) {
-            throw new IllegalStateException("CPF já cadastrado.");
-        }
-        pessoas.put(cpf, new PessoaImpl(cpf, nome, habilidades));
-    }
-
-    public String exibirPessoa(String cpf) throws NoSuchElementException {
-        Pessoa pessoa = pessoas.get(cpf);
-        if (pessoa == null) {
-            throw new NoSuchElementException("CPF não encontrado.");
-        }
-        return pessoa.exibir();
-    }
-
-    public void alterarNomePessoa(String cpf, String novoNome) throws NoSuchElementException {
-        Pessoa pessoa = pessoas.get(cpf);
-        if (pessoa == null) {
-            throw new NoSuchElementException("CPF não encontrado.");
-        }
-        pessoa.alterarNome(novoNome);
-    }
-
-    public void alterarHabilidadesPessoa(String cpf, String[] novasHabilidades) throws NoSuchElementException {
-        Pessoa pessoa = pessoas.get(cpf);
-        if (pessoa == null) {
-            throw new NoSuchElementException("CPF não encontrado.");
-        }
-        pessoa.alterarHabilidades(novasHabilidades);
-    }
-
-    public void removerPessoa(String cpf) throws NoSuchElementException {
-        Pessoa pessoa = pessoas.get(cpf);
-        if (pessoa == null) {
-            throw new NoSuchElementException("CPF não encontrado.");
-        }
+    public void remove(String cpf) {
         pessoas.put(cpf, null);
-    }
-
-    public void adicionarComentarioPessoa(String cpf, String comentario, String autorCpf) throws NoSuchElementException {
-        Pessoa pessoa = pessoas.get(cpf);
-        Pessoa autor = pessoas.get(autorCpf);
-        if (pessoa == null || autor == null) {
-            throw new NoSuchElementException("CPF não encontrado.");
-        }
-        pessoa.adicionarComentario(comentario, autor);
-    }
-
-    public String listarComentariosPessoa(String cpf) throws NoSuchElementException {
-        Pessoa pessoa = pessoas.get(cpf);
-        if (pessoa == null) {
-            throw new NoSuchElementException("CPF não encontrado.");
-        }
-        return pessoa.listarComentarios();
     }
 
 }
