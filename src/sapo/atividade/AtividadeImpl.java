d479f1912f235a3fae835b0c434bf4a5ba2199e4
kenbme <guilhermekenned1@gmail.com>
2022-08-19 14:05:30 -0300

    Removido import não usado, escrita melhorada.

--- src/sapo/atividade/AtividadeImpl.java
+++ src/sapo/atividade/AtividadeImpl.java
@@ -1,22 +1,23 @@
 package sapo.atividade;
 
-import sapo.pessoa.Pessoa;
-
 public class AtividadeImpl implements Atividade {
 
     private final String ID;
     private final String nome;
     private String descricao;
     private String cpfResponsavel;
-    private boolean encerrada = false;
-    private boolean ativada = true;
+    private boolean encerrada;
+    private boolean ativada;
 
     public AtividadeImpl(String ID, String nome, String descricao, String cpfResponsavel) {
         this.ID = ID;
         this.nome = nome;
         this.descricao = descricao;
         this.cpfResponsavel = cpfResponsavel;
+        this.encerrada = false;
+        this.ativada = true;
     }
+
     // TODO Checar erros, verificações e outras especificidades
     @Override
     public void encerrar(){
@@ -30,30 +31,37 @@ public class AtividadeImpl implements Atividade {
     public void desativar(){
         if (!encerrada){
             ativada = false;
-        } throw new IllegalStateException("Atividade já está desativada.");
+        }
+        throw new IllegalStateException("Atividade já está desativada.");
     }
 
     @Override
     public void reabrir(){
-        if (ativada != false){
+        if (!ativada){
             ativada = true;
-        } throw new IllegalStateException("Atividade já está ativada.");
+        }
+        throw new IllegalStateException("Atividade já está ativada.");
     }
 
     @Override
     public void alterarDescricao(String novaDescricao) { this.descricao = novaDescricao; }
+
     @Override
     public void alterarResponsavel(String cpfResponsavelNovo){ this.cpfResponsavel = cpfResponsavelNovo; }
+
     @Override
     public String getResponsavel(){
         return this.cpfResponsavel;
     }
+
     @Override
     public String getNome(){
         return this.nome;
     }
+
     @Override
     public String getDescricao(){
         return this.descricao;
     }
+
 }
