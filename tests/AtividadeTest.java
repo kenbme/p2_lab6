d0bdc163a5e60593d4a1715ae253052545dc8694
kenbme <guilhermekenned1@gmail.com>
2022-08-19 13:24:20 -0300

    Teste simples de cadastrar Atividade.

--- tests/AtividadeTest.java
+++ tests/AtividadeTest.java
@@ -3,5 +3,10 @@ import org.junit.jupiter.api.Test;
 
 public class AtividadeTest extends BaseTest {
 
+    @Test
+    void cadastrarAtividadeTest1() {
+        String descricao = "Atividade de estudo de OO, considerando alunos com experiência de programação e uso da linguagem Java.";
+        Assertions.assertEquals("STD-1", facade.cadastrarAtividade("Estudar OO", descricao, cpf1));
+    }
 
 }
