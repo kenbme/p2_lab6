ccad47d92a49643e72bc607d24a4bf452bb51843
jgluiggi <jgluiggi@protonmail.com>
2022-08-21 16:04:59 -0300

    Adicionada verificação para todas as entradas no controller.

--- src/sapo/atividade/AtividadeController.java
+++ src/sapo/atividade/AtividadeController.java
@@ -20,26 +20,39 @@ public class AtividadeController {
     }
 
     public void encerrarAtividade(String atividadeId) {
+        validador.valida(atividadeId);
+        validador.validaID(atividadeId);
         atividadeService.encerrarAtividade(atividadeId);
     }
 
     public void desativarAtividade(String atividadeId) {
+        validador.valida(atividadeId);
+        validador.validaID(atividadeId);
         atividadeService.desativarAtividade(atividadeId);
     }
 
     public void reabrirAtividade(String atividadeId) {
+        validador.valida(atividadeId);
+        validador.validaID(atividadeId);
         atividadeService.reabrirAtividade(atividadeId);
     }
 
     public String exibirAtividade(String atividadeId) {
+        validador.valida(atividadeId);
+        validador.validaID(atividadeId);
         return atividadeService.exibirAtividade(atividadeId);
     }
 
     public void alterarDescricaoAtividade(String atividadeId, String descricao) {
+        validador.valida(atividadeId);
+        validador.validaID(atividadeId);
+        validador.valida(descricao);
         atividadeService.alterarDescricaoAtividade(atividadeId, descricao);
     }
 
     public void alterarResponsavelAtividade(String atividadeId, String cpf) {
+        validador.valida(atividadeId);
+        validador.validaID(atividadeId);
         validador.validaCpf(cpf);
         atividadeService.alterarResponsavelAtividade(atividadeId, cpf);
     }
