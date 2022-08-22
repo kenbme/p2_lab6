415c82218237898d253f46fa39a29441acbdc6d9
kenbme <guilhermekenned1@gmail.com>
2022-08-22 18:09:41 -0300

    Criando mais testes de Tarefa.

--- tests/TarefaTest.java
+++ tests/TarefaTest.java
@@ -4,21 +4,46 @@ import org.junit.jupiter.api.Test;
 public class TarefaTest extends BaseTest {
 
     @Test
-    void cadastrarTarefaTest1() {
+    void cadastrarTarefa1() {
         Assertions.assertEquals("STD-0-1", facade.cadastrarTarefa(atividade1, "Montar documentação.", new String[]{"OO"}));
     }
 
     @Test
     void exibirTarefa1() {
+        String id = facade.cadastrarTarefa(atividade1, "Aula sobre herança", new String[]{"OO", "Herança", "Professor"});
+        facade.associarPessoaTarefa(cpf1, id);
+        facade.adicionarHorasTarefa(id, 20);
         Assertions.assertEquals(
-                "Preparar material de estudo - STD-0-0\n" +
+                "Aula sobre herança - STD-0-1\n" +
                         "- Estudar OO\n" +
-                        "OO\n" +
-                        "(0 hora(s) executada(s)\n" +
+                        "OO, Herança, Professor\n" +
+                        "(20 hora(s) executada(s))\n" +
                         "===\n" +
-                        "Equipe:"
+                        "Equipe:\n" +
+                        "Matheus Canella – 222.222.222-22"
                 ,
-                facade.exibirTarefa("STD-0-0"));
+                facade.exibirTarefa(id));
+    }
+
+    @Test
+    void exibirTarefaGerencial1() {
+        String id = facade.cadastrarTarefaGerencial(atividade1, "Tarefa gerencial",
+                new String[]{"OO", "Herança", "Professor"}, new String[]{tarefa1});
+        facade.associarPessoaTarefa(cpf1, id);
+        facade.adicionarHorasTarefa(id, 20);
+        Assertions.assertEquals(
+                "Tarefa gerencial - STD-0-1\n" +
+                        "- Estudar OO\n" +
+                        "OO, Herança, Professor\n" +
+                        "(20 hora(s) executada(s))\n" +
+                        "===\n" +
+                        "Equipe:\n" +
+                        "Matheus Canella – 222.222.222-22" +
+                        "\n===\n" +
+                        "Tarefas:\n" +
+                        "- Preparar material de estudo - STD-0-0"
+                ,
+                facade.exibirTarefa(id));
     }
 
 }
