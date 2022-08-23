df3f709404060adbea8d5382d3ac8657ea7f0d33
kenbme <guilhermekenned1@gmail.com>
2022-08-23 01:12:23 -0300

    Mais testes de Tarefa e Tarefa Gerencial.

--- tests/TarefaTest.java
+++ tests/TarefaTest.java
@@ -9,6 +9,12 @@ public class TarefaTest extends BaseTest {
     }
 
     @Test
+    void cadastrarTarefaGerencial1() {
+        Assertions.assertEquals("STD-0-1", facade.cadastrarTarefaGerencial(atividade1, "Tarefa gerencial",
+                new String[]{"OO", "Herança", "Professor"}, new String[]{tarefa1}));
+    }
+
+    @Test
     void exibirTarefa1() {
         String id = facade.cadastrarTarefa(atividade1, "Aula sobre herança", new String[]{"OO", "Herança", "Professor"});
         facade.associarPessoaTarefa(cpf1, id);
@@ -24,6 +30,36 @@ public class TarefaTest extends BaseTest {
                 ,
                 facade.exibirTarefa(id));
     }
+
+    @Test
+    void exibirTarefa2() {
+        String id = facade.cadastrarTarefa(atividade1, "Aula sobre herança", new String[]{"OO", "Herança", "Professor"});
+        facade.adicionarHorasTarefa(id, 20);
+        Assertions.assertEquals(
+                "Aula sobre herança - STD-0-1\n" +
+                        "- Estudar OO\n" +
+                        "OO, Herança, Professor\n" +
+                        "(20 hora(s) executada(s))\n" +
+                        "===\n" +
+                        "Equipe:"
+                ,
+                facade.exibirTarefa(id));
+    }
+
+    @Test
+    void exibirTarefa3() {
+        String id = facade.cadastrarTarefa(atividade1, "Aula sobre herança", new String[]{"OO", "Herança", "Professor"});
+        Assertions.assertEquals(
+                "Aula sobre herança - STD-0-1\n" +
+                        "- Estudar OO\n" +
+                        "OO, Herança, Professor\n" +
+                        "(0 hora(s) executada(s))\n" +
+                        "===\n" +
+                        "Equipe:"
+                ,
+                facade.exibirTarefa(id));
+    }
+
 
     @Test
     void exibirTarefaGerencial1() {
@@ -45,5 +81,20 @@ public class TarefaTest extends BaseTest {
                 ,
                 facade.exibirTarefa(id));
     }
+
+    @Test
+    void entradasNullInvalidas() {
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> facade.cadastrarTarefa(null, null, null));
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> facade.cadastrarTarefa("STD-0", null,null));
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> facade.cadastrarTarefa("STD-0", "a", null));
+    }
+
+    @Test
+    void entradasVaziasInvalidas() {
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> facade.cadastrarTarefa("", "", new String[]{""}));
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> facade.cadastrarTarefa("STD-0", "", new String[]{""}));
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> facade.cadastrarTarefa("STD-0", "a", new String[]{""}));
+    }
+
 
 }
